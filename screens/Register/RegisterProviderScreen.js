80668e7049b3a06a4e5bb2c3dd5b4c3160167301
carotesel <caroteselmann@gmail.com>
2022-06-10 10:19:24 -0300

    cambio views figmax

--- screens/Register/RegisterProviderScreen.js
+++ screens/Register/RegisterProviderScreen.js
@@ -1,11 +1,19 @@
-import { View, Text, TextInput, StatusBar, StyleSheet, TouchableOpacity } from "react-native";
-import React, { useState } from 'react';
-
+import {
+  View,
+  Text,
+  TextInput,
+  StatusBar,
+  StyleSheet,
+  TouchableOpacity,
+} from "react-native";
+import React, { useState } from "react";
+import DropDownPicker from "react-native-dropdown-picker";
 
 const RegisterProviderScreen = () => {
-  
+  const tiposLocker = ["Chico", "Mediano", "Grande"];
+
   const [data, setData] = useState({
-    nombre: undefined, 
+    nombre: undefined,
     apellido: undefined,
     email: undefined,
     dni: undefined,
@@ -15,41 +23,106 @@ const RegisterProviderScreen = () => {
     ciudad: undefined,
     codPostal: undefined,
     emailComercio: undefined,
+    razonSocial: undefined,
+    CUIT: undefined,
   });
 
-  const onChangeInput = (e, name) => { 
+  const onChangeInput = (e, name) => {
     setData({
-      ...data, 
-      [name]: e.nativeEvent.text
+      ...data,
+      [name]: e.nativeEvent.text,
     });
-  }
+  };
 
-  const registrar = () => { 
-    console.log(data)
-  }
-  
+  const registrar = () => {
+    console.log(data);
+  };
+
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Datos del titular</Text>
-      <TextInput style={styles.textInput} placeholder="Nombre" name="nombre" onChange={(e) => onChangeInput(e, "nombre")}/>
-      <TextInput style={styles.textInput} placeholder="Apellido" name="apellido" onChange={(e) => onChangeInput(e, "apellido")}/>
-      <TextInput style={styles.textInput} placeholder="Email" name="email" onChange={(e) => onChangeInput(e, "email")}/>
-      <TextInput style={styles.textInput} placeholder="DNI" name="dni" onChange={(e) => onChangeInput(e, "dni")}/>
+      <TextInput
+        style={styles.textInput}
+        placeholder="Nombre"
+        name="nombre"
+        onChange={(e) => onChangeInput(e, "nombre")}
+      />
+      <TextInput
+        style={styles.textInput}
+        placeholder="Apellido"
+        name="apellido"
+        onChange={(e) => onChangeInput(e, "apellido")}
+      />
+      <TextInput
+        style={styles.textInput}
+        placeholder="Email"
+        name="email"
+        onChange={(e) => onChangeInput(e, "email")}
+      />
+      <TextInput
+        style={styles.textInput}
+        placeholder="DNI"
+        name="dni"
+        onChange={(e) => onChangeInput(e, "dni")}
+      />
 
       <Text style={styles.text}>Datos del negocio</Text>
-      <TextInput style={styles.textInput} placeholder="Nombre del comercio" name="nombreComercio" onChange={(e) => onChangeInput(e, "nombreComercio")}/>
-      <TextInput style={styles.textInput} placeholder="Dirección" name="calle" onChange={(e) => onChangeInput(e, "calle")}/>
-      <TextInput style={styles.textInput} placeholder="Altura" name="altura" onChange={(e) => onChangeInput(e, "altura")}/>
-      <TextInput style={styles.textInput} placeholder="Ciudad" name="ciudad" onChange={(e) => onChangeInput(e, "ciudad")}/>
-      <TextInput style={styles.textInput} placeholder="Código Postal" name="codPostal" onChange={(e) => onChangeInput(e, "codPostal")}/>
-      <TextInput style={styles.textInput} placeholder="Email del comercio" name="emailComercio" onChange={(e) => onChangeInput(e, "emailComercio")}/>
+      <TextInput
+        style={styles.textInput}
+        placeholder="Nombre del comercio"
+        name="nombreComercio"
+        onChange={(e) => onChangeInput(e, "nombreComercio")}
+      />
+      <TextInput
+        style={styles.textInput}
+        placeholder="Dirección"
+        name="calle"
+        onChange={(e) => onChangeInput(e, "calle")}
+      />
+      <TextInput
+        style={styles.textInput}
+        placeholder="Altura"
+        name="altura"
+        onChange={(e) => onChangeInput(e, "altura")}
+      />
+      <TextInput
+        style={styles.textInput}
+        placeholder="Ciudad"
+        name="ciudad"
+        onChange={(e) => onChangeInput(e, "ciudad")}
+      />
+      <TextInput
+        style={styles.textInput}
+        placeholder="Código Postal"
+        name="codPostal"
+        onChange={(e) => onChangeInput(e, "codPostal")}
+      />
+      <TextInput
+        style={styles.textInput}
+        placeholder="Email del comercio"
+        name="emailComercio"
+        onChange={(e) => onChangeInput(e, "emailComercio")}
+      />
+      <TextInput
+        style={styles.textInput}
+        placeholder="Razón Social del comercio"
+        name="razonSocial"
+        onChange={(e) => onChangeInput(e, "razonSocial")}
+      />
+      <TextInput
+        style={styles.textInput}
+        placeholder="CUIT"
+        name="CUIT"
+        onChange={(e) => onChangeInput(e, "CUIT")}
+      />
 
-      <TouchableOpacity style={styles.orangebutton}>
+      <Text style={styles.text}>Elija sus lockers</Text>
+      
+      <TouchableOpacity style={styles.orangebutton} onPress={registrar}>
         <View style={styles.centerText}>
-        <Text style={styles.text} onPress={registrar}>Registrar</Text>
+          <Text style={styles.text}>Registrarme</Text>
         </View>
       </TouchableOpacity>
-    
 
       <Text style={styles.text}>Ya tenés una cuenta? Iniciar Sesión</Text>
 
@@ -61,36 +134,38 @@ const RegisterProviderScreen = () => {
 export default RegisterProviderScreen;
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: "#051E44",
-      alignItems: "center",
-      justifyContent: "center",
-    },
-    textInput: {
-      borderWidth: 1,
-      padding: 15,
-      width: "85%",
-      borderRadius: 8,
-      backgroundColor: "#fff",
-      marginTop: 8,
-    },
-    text: {
-      color: "#ffffff",
-      alignItems: "center",
-      justifyContent: "center",
-    },
-    centerText: {
-      alignItems: "center",
-    },
-    orangebutton: {
-      backgroundColor: "#F1230B",
-      borderWidth: 1,
-      padding: 15,
-      width: "35%",
-      borderRadius: 8,
-      marginTop: 8,
-      marginBottom: 8,
-    },
-  });
-  
+  container: {
+    flex: 1,
+    backgroundColor: "#051E44",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  textInput: {
+    borderWidth: 1,
+    padding: 15,
+    width: "85%",
+    borderRadius: 8,
+    backgroundColor: "#fff",
+    marginTop: 8,
+  },
+  text: {
+    color: "#ffffff",
+    alignItems: "center",
+    justifyContent: "center",
+    fontSize: 20,
+    marginBottom: 8,
+    marginTop: 12,
+  },
+  centerText: {
+    alignItems: "center",
+  },
+  orangebutton: {
+    backgroundColor: "#DF4F1A",
+    borderWidth: 1,
+    padding: 15,
+    width: "65%",
+    borderRadius: 8,
+    marginTop: 8,
+    marginBottom: 8,
+  },
+});
--- screens/Register/RegisterScreen.js
+++ screens/Register/RegisterScreen.js
@@ -1,37 +1,92 @@
-import { View, Text, Button, StyleSheet, Image } from 'react-native'
-import React from 'react'
-import { useNavigation } from '@react-navigation/native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+  TextInput,
+} from "react-native";
+import React from "react";
+import { useNavigation } from "@react-navigation/native";
 
 const RegisterScreen = () => {
   const navigation = useNavigation();
-  
+
   return (
     <View style={styles.container}>
       
-      <Image source={require('../Register/images/Logo2.png')}/>
-      
-      <Text>Registrate!</Text>
+      <Image style={styles.image} source={require("../Register/images/LOGO.png")} />
 
-      <Button title='Soy Cliente'/>
+      <TextInput style={styles.textInput} placeholder="Nombre" name="CUIT" />
 
-      <Button title='Soy Proveedor!' style={styles.orangebutton} onPress={() => navigation.navigate("RegisterProvider")}/>
+      <TextInput style={styles.textInput} placeholder="Apellido" name="CUIT" />
+
+      <TextInput style={styles.textInput} placeholder="Email" name="CUIT" />
+
+      <TextInput style={styles.textInput} placeholder="Nombre de Usuario" name="CUIT" />
+
+      <TextInput style={styles.textInput} placeholder="Contraseña" name="CUIT" />
+
+      <TextInput style={styles.textInput} placeholder="Confirmar contraseña" name="CUIT" />
+
+
+
+      <TouchableOpacity
+        style={styles.orangebutton}
+        onPress={() => navigation.navigate("RegisterProvider")}
+      >
+        <View style={styles.centerText}>
+          <Text style={styles.text}>Registrarme</Text>
+        </View>
+      </TouchableOpacity>
     </View>
   );
-}
-
-
+};
 
 export default RegisterScreen;
 
-const styles = StyleSheet.create({ 
+const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#051E44",
     alignItems: "center",
+    justifyContent: "center",},
+ 
+    title: {
+    color: "#ffffff",
+    alignItems: "center",
     justifyContent: "center",
+    fontSize: 28,
+    marginBottom: 20,
+  },
+  text: {
+    color: "#ffffff",
+    alignItems: "center",
+    justifyContent: "center",
+    fontSize: 18,
+  },
+  centerText: {
+    alignItems: "center",
   },
   orangebutton: {
-    backgroundColor: "#F1230B",
+    backgroundColor: "#DF4F1A",
     borderWidth: 1,
+    padding: 15,
+    width: "65%",
+    borderRadius: 8,
+    marginTop: 20,
+    marginBottom: 8,
   },
+  textInput: {
+    borderWidth: 1,
+    padding: 15,
+    width: "85%",
+    borderRadius: 8,
+    backgroundColor: "#fff",
+    marginTop: 8,
+    marginBottom: 8,
+  },
+  image: {
+    marginBottom: 25,
+  }
 });
